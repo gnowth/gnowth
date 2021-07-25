25804b9a6f222b1b7e675ef5ca4f5855ea9e1d6e
Wong, Thierry <thierry.wong10@auspost.com.au>
2021-07-25 16:50:33 +1000

    feat: eslint to support mdx file

--- packages/config-eslint/hybrid-react.js
+++ packages/config-eslint/hybrid-react.js
@@ -1,10 +1,15 @@
 const javascript = require('./javascript-react')
+const markdown = require('./markdown-react')
 const typescript = require('./typescript-react')
 
 module.exports = {
   ...javascript,
 
   overrides: [
+    {
+      ...markdown,
+      files: ['*.mdx'],
+    },
     {
       ...typescript,
       files: ['*.ts', '*.tsx'],
---
+++ packages/config-eslint/markdown-react.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    mongo: true,
+    node: true,
+    serviceworker: true,
+    worker: true,
+  },
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:mdx/recommended',
+    'plugin:promise/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'import',
+    'jest',
+    'jsx-a11y',
+    'mdx',
+    'prettier',
+    'promise',
+    'react',
+    'react-hooks',
+  ],
+
+  settings: {
+    'import/extensions': ['.md', '.mdx'],
+    'mdx/code-blocks': true,
+    'mdx/language-mapper': {},
+  },
+
+  rules: {
+    'prettier/prettier': 'error',
+
+    /** Note: React is no longer required if babel is provided
+     *  Link: https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+     */
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
