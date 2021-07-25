dba51b40bdc8f6e0eb41c9f8a83ad0fac7390b41
Wong, Thierry <thierry.wong10@auspost.com.au>
2021-07-25 17:35:58 +1000

    feat: version on homepage. added changelog page and link to it

--- packages/boilerplate-nextjs/next.config.js
+++ packages/boilerplate-nextjs/next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+
+module.exports = withMDX({
   images: {
     loader: 'imgix',
     path: '',
   },
+  pageExtensions: ['tsx', 'mdx'],
   reactStrictMode: true,
-}
+})
