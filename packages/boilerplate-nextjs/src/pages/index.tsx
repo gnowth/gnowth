bf96e3e665130150fae8ab15db368e9fa7326050
Wong, Thierry <thierry.wong10@auspost.com.au>
2022-02-20 16:18:36 +1100

    chore: update packages

--- packages/boilerplate-nextjs/src/pages/index.tsx
+++ packages/boilerplate-nextjs/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 
 import packageJson from '../../package.json'
@@ -65,10 +64,7 @@ export default function Home(): ReactElement {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by
         </a>
       </footer>
     </div>
