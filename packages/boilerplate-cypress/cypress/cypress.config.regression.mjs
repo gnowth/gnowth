09fb9102e27ebc01e4d5f4ed41fad08437510edf
thierry <unknown>
2022-07-31 12:38:27 +1000

    chore: restructure scripts

--- packages/boilerplate-cypress/cypress/cypress.config.regression.mjs
+++ packages/boilerplate-cypress/cypress/cypress.config.regression.mjs
@@ -1,24 +1,10 @@
 import { defineConfig } from 'cypress'
-import { deleteAsync } from 'del'
 
-// Note: cypress does not support module from 'del' library [cypress v10.3.1 / del v7.0.0]
+import { e2eConfigs } from './cypress.config.mock.mjs'
+
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASEURL,
-    screenshotsFolder: 'cypress/media',
-    specPattern: 'cypress/integration/*.ts',
-    supportFile: 'cypress/support/page-nextjs.ts',
-    videoCompression: false,
-    videosFolder: 'cypress/media',
-    setupNodeEvents: (on) => {
-      /** Note: Delete video if test succeed.
-       *  Link: https://github.com/cypress-io/cypress/issues/2522#issuecomment-749316813
-       */
-      on('after:spec', (spec, results) => {
-        if (results.stats.failures === 0 && results.video) {
-          return deleteAsync(results.video)
-        }
-      })
-    },
+    ...e2eConfigs,
+    excludeSpecPattern: 'cypress/integration/*.mock.ts',
   },
 })
--- packages/boilerplate-cypress/cypress/cypress.config.smoke.mjs
+++ packages/boilerplate-cypress/cypress/cypress.config.smoke.mjs
@@ -1,23 +1,10 @@
 import { defineConfig } from 'cypress'
-import { deleteAsync } from 'del'
+
+import { e2eConfigs } from './cypress.config.mock.mjs'
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASEURL,
-    screenshotsFolder: 'cypress/media',
+    ...e2eConfigs,
     specPattern: 'cypress/integration/*.smoke.ts',
-    supportFile: 'cypress/support/page-nextjs.ts',
-    videoCompression: false,
-    videosFolder: 'cypress/media',
-    setupNodeEvents: (on) => {
-      /** Note: Delete video if test succeed.
-       *  Link: https://github.com/cypress-io/cypress/issues/2522#issuecomment-749316813
-       */
-      on('after:spec', (spec, results) => {
-        if (results.stats.failures === 0 && results.video) {
-          return deleteAsync(results.video)
-        }
-      })
-    },
   },
 })
