4c3a27b145301d5486f1635b4b5f5fe073b498a6
thierry <>
2022-07-31 12:38:27 +1000

    chore: restructure scripts

---
+++ packages/boilerplate-cypress/cypress/cypress.config.mock.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress'
+import { deleteAsync } from 'del'
+
+// Note: cypress does not support es6 module from 'del' library [cypress v10.3.1 / del v7.0.0]
+export const e2eConfigs = {
+  baseUrl: process.env.CYPRESS_BASEURL,
+  screenshotsFolder: 'cypress/media',
+  specPattern: 'cypress/integration/*.ts',
+  supportFile: 'cypress/support/page-nextjs.ts',
+  videoCompression: false,
+  videosFolder: 'cypress/media',
+  setupNodeEvents: (on) => {
+    /** Note: Delete video if test succeed.
+     *  Link: https://github.com/cypress-io/cypress/issues/2522#issuecomment-749316813
+     */
+    on('after:spec', (spec, results) => {
+      if (results.stats.failures === 0 && results.video) {
+        return deleteAsync(results.video)
+      }
+    })
+  },
+}
+
+export default defineConfig({ e2e: e2eConfigs })
