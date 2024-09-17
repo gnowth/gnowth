27add7f2d987b6c2d8f62c22261ae768d9587777
thierry <gnowth@users.noreply.github.com>
2024-09-16 21:13:10 -0400

    chore: remove sonarjs unnecessary awaiit

--- packages/boilerplate-nextjs/src/pages/users/generated/[slug].tsx
+++ packages/boilerplate-nextjs/src/pages/users/generated/[slug].tsx
@@ -1,21 +1,10 @@
 import { PageGeneratedClient } from '@gnowth/app-users'
-import { GetStaticPropsContext, NextPage } from 'next'
+import { NextPage } from 'next'
 import { ComponentProps, ComponentType } from 'react'
 
-export async function getStaticPaths() {
-  const paths = await PageGeneratedClient.staticPaths()
-
-  return { fallback: false, paths }
-}
-
-export async function getStaticProps(context: GetStaticPropsContext) {
-  const props = await PageGeneratedClient.staticProps(context)
-
-  return { props }
-}
-
+export const getStaticPaths = PageGeneratedClient.staticPaths
+export const getStaticProps = PageGeneratedClient.staticProps
 const Page: { Layout?: ComponentType | null } & NextPage<ComponentProps<typeof PageGeneratedClient>> =
   PageGeneratedClient
 Page.Layout = null
-
 export default Page
