2ae9d93b5bb235e7a0def4c54d5c50508dc4dfcc
Thierry Wong <gnowth@users.noreply.github.com>
2024-10-14 21:39:20 -0400

    chore: cleanup packages (#629)
    * chore: remove lib animation
    
    * chore: remove lib query

--- packages/lib-animation/src/exports.ts
+++
@@ -1,3 +0,0 @@
-export * from './use-animation-delay-props'
-export * from './use-animation-delay-ready'
-export * from './use-animation-delay-value'
--- packages/lib-application/src/use-app-who-am-i.ts
+++ packages/lib-application/src/use-app-who-am-i.ts
@@ -1,7 +1,7 @@
-import { QueryResource } from '@gnowth/lib-query'
 import { useContext } from 'react'
 
 import { ContextEnvironment } from './context-environment'
+import { QueryResource } from './queries/query-resource'
 
 export function useAppWhoAmI(): [QueryResource | null, (resource: QueryResource | null) => void] {
   const contextEnvironment = useContext(ContextEnvironment)
