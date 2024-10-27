b9b31039adca42f1e3fdc1edfcf80173cecdd27d
Thierry Wong <gnowth@users.noreply.github.com>
2024-10-27 12:41:44 -0400

    feat: add react select (#681)
    * feat: add react select
    
    * fix: wrap react select

--- packages/lib-view/src/input/input-select.tsx
+++ packages/lib-view/src/input/input-select.tsx
@@ -1,10 +1,32 @@
 import { FunctionComponent } from 'react'
+import Select from 'react-select'
 
+import { UIBox } from '../ui/ui-box'
+import { ChangeEventHandler } from './use-value'
+
+type Option = { label: string; value: unknown }
 export type PropsInputSelect = {
   as?: string
+  id?: string
+  menuPlacement?: 'auto' | 'bottom' | 'top'
   name?: string
+  onChange?: ChangeEventHandler<Option | null>
+  options?: Option[]
+  placeholder?: string
+  value?: Option | null
 }
 
-export const InputSelect: FunctionComponent<PropsInputSelect> = (props) => (
-  <input className="input-select" data-testid="view-input-select" name={props.name} />
-)
+export const InputSelect: FunctionComponent<PropsInputSelect> = (props) => {
+  return (
+    <UIBox className="input-select" data-testid="view-input-select">
+      <Select
+        menuPlacement={props.menuPlacement}
+        name={props.name}
+        onChange={(newValue) => props.onChange?.({ target: { value: newValue } })}
+        options={props.options}
+        placeholder={props.placeholder}
+        value={props.value}
+      />
+    </UIBox>
+  )
+}
